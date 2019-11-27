9bd0a782a2cbdb8d1e64a62031451a9d1baed4a3
leeraaz17 <liraj@banbagroup.com>
2019-11-27 17:09:59 +0545

    Wrote spec for displaying messages.

--- spec/javascripts/hello_vue.spec.js
+++ spec/javascripts/hello_vue.spec.js
@@ -1,14 +1,33 @@
-import { shallowMount } from "@vue/test-utils";
+import { shallowMount, mount } from "@vue/test-utils";
 import App from "@/app/javascript/app.vue";
 import { createRenderer } from "vue-server-renderer";
 
 describe("app", () => {
-    it("success matches snapshot", () => {
-      const renderer = createRenderer();
-			const wrapper = shallowMount(App);
-      renderer.renderToString(wrapper.vm, (err, str) => {
-        if (err) throw new Error(err);
-        expect(str).toMatchSnapshot();
-      });
+  it("success matches snapshot", () => {
+    const renderer = createRenderer();
+    const wrapper = shallowMount(App);
+    renderer.renderToString(wrapper.vm, (err, str) => {
+      if (err) throw new Error(err);
+      expect(str).toMatchSnapshot();
     });
+  });
 });
+
+describe('App', () => {
+  test('is a Vue instance', () => {
+    const wrapper = mount(App)
+    expect(wrapper.isVueInstance()).toBeTruthy()
+    // expect(wrapper.attributes().message).toBe("Hello Vue!")
+    // const hellos = JSON.parse(JSON.stringify(wrapper))
+    // console.log(wrapper.html())
+    // console.log(hellos)
+  })
+
+  it("should display props.ms when passed", () => {
+    // const msg = "This is something new";
+    // const wrapper = shallowMount(App, {
+    //   propsData: { msg }
+    // });
+    // expect(wrapper.text()).toMatch(msg);
+  })
+})
--- spec/javascripts/jesttesting/TodoList.spec.js
+++ spec/javascripts/jesttesting/TodoList.spec.js
@@ -1,10 +1,12 @@
-import { shallowMount } from '@vue/test-utils';
+import { shallowMount, mount } from '@vue/test-utils';
 import TodoList from '@/app/javascript/packs/jesttesting/TodoList.vue';
 
 describe('TodoList', () => {
   it('should render to a snapshot when list is empty', () => {
     const propsData = { todos: [] }
     const wrapper = shallowMount(TodoList, { propsData })
+    // const jpt = {beef: "hehe", bacon: "haha"}
+    // console.log(JSON.parse(JSON.stringify(jpt)))
     expect(wrapper.element).toMatchSnapshot()
   })
 
@@ -12,5 +14,10 @@ describe('TodoList', () => {
     const propsData = { todos: [{ id: 1, text: 'Todo #1' }] }
     const wrapper = shallowMount(TodoList, { propsData })
     expect(wrapper.element).toMatchSnapshot()
+  })
+
+  test('is a Vue instance', () => {
+    const wrapper = mount(TodoList)
+    expect(wrapper.isVueInstance()).toBeTruthy()
   })
 })
