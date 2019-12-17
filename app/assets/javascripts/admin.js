8204bb1e6a7359dea6faaeab5e26ef9df8f83ad3
leeraaz17 <liraj@banbagroup.com>
2019-12-17 17:21:40 +0545

    Added new adminlte panel for admins.

---
+++ app/assets/javascripts/admin.js
@@ -0,0 +1,26 @@
+// This is a manifest file that'll be compiled into application.js, which will include all the files
+// listed below.
+//
+// Any JavaScript/Coffee file within this directory, lib/assets/javascripts, or any plugin's
+// vendor/assets/javascripts directory can be referenced here using a relative path.
+//
+// It's not advisable to add code directly here, but if you do, it'll appear at the bottom of the
+// compiled file. JavaScript code in this file should be added after the last require_* statement.
+//
+// Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
+// about supported directives.
+//
+//= require rails-ujs
+//= require activestorage
+//= require turbolinks
+//= require trix
+
+
+//= require "admin/jquery-easing/jquery.easing.min.js"
+//= require "admin/jquery/jquery.min.js"
+//= require "admin/bootstrap/js/bootstrap.bundle.min.js"
+//= require "admin/chart.js/Chart.min.js"
+//= require "admin/datatables/dataTables.bootstrap4.js"
+//= require "admin/js/sb-admin.min.js"
+//= require "admin/js/demo/datatables-demo.js"
+//= require "admin/js/demo/chart-area-demo.js"
--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -11,4 +11,18 @@ Rails.application.config.assets.paths << Rails.root.join('node_modules')
 # Precompile additional assets.
 # application.js, application.css, and all non-JS/CSS in the app/assets
 # folder are already added.
+assets = [
+  "admin/pages.css",
+  "admin/jquery-easing/*.js",
+  "admin/jquery-easing/jquery.easing.js",
+  "admin/js/*.js",
+  "admin/js/demo/*.js"
+]
+Rails.application.config.assets.precompile += assets
+
+Dir.glob(Rails.root.join("app", "assets", "plugins", "**")).each do |path|
+  Rails.application.config.assets.paths << path
+end
+
+
 Rails.application.config.assets.precompile += %w( admin.js admin.css index.js)
